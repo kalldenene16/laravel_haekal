1c12d6f4c3bc686e91e8178713cc75f23f378c73
unknown <irsanhaekal6@gmail.com>
2023-10-24 14:01:41 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Haekal Irsan Sembiring",
+        "email" => "irsanhaekal6@gmail.com",
+        "image" => "sucrose.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
